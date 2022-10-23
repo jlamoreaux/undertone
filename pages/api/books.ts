64ee460909755c12ec2ac881cdc4993e276b80f6
Jordan Lamoreaux <jlamoreaux@atlassian.com>
2022-10-23 07:18:54 -0500

    fix return status for book api

--- pages/api/books.ts
+++ pages/api/books.ts
@@ -6,13 +6,16 @@ const getBooks = async (req: NextApiRequest, res: NextApiResponse) => {
   const jsonDirectory = path.join(process.cwd(), "json");
   const fileContents = await fs.readFile(jsonDirectory + "/books.json", "utf8");
 
-  const { id } = req.query
+  const id = req.query.id;
+  if (isNaN(Number(id))) {
+    res.status(400).end();
+  }
   if (id) {
     const book = JSON.parse(fileContents)[Number(id)];
     if (book) {
       res.status(200).json(book);
     } else {
-      res.status(400).end();
+      res.status(404).end();
     }
     return;
   }
