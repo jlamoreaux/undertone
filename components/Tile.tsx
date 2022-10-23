dfec70ef16f35824ff6fc2664b37894eb288c013
Jordan Lamoreaux <jlamoreaux@atlassian.com>
2022-10-23 07:12:22 -0500

    update chapter tiles to have action when clicked

--- components/Tile.tsx
+++ components/Tile.tsx
@@ -1,5 +1,8 @@
 import { CSSProperties, FC, useState } from "react";
 import Link from "next/link";
+import { Box, List, Popover, ThemeIcon } from "@mantine/core";
+import { IconBook, IconExternalLink, IconPencil } from "@tabler/icons";
+import { useClickOutside } from "@mantine/hooks";
 
 
 export interface Book {
@@ -13,10 +16,10 @@ export interface Book {
   chaptersRead?: number[];
 }
 
-type BaseTileProps = {
+type BaseTileProps = React.DOMAttributes<HTMLDivElement> & {
   label: string;
   style?: CSSProperties;
-}
+};
 
 type BookTileProps = {
   book: Book;
@@ -24,37 +27,48 @@ type BookTileProps = {
 }
 
 type ChapterTileProps = {
+  bookTitle: string;
   chapter: number;
   isRead: boolean;
-}
+  isRecording: boolean;
+  isSelected: boolean;
+  beginRecording: () => void;
+  toggleTileSelected: (chapter: number)=> void;
+};
 
 export enum BACKGROUND_COLOR {
   UNREAD = "var(--mantine-color-gray-6)",
   READ = "var(--mantine-color-gray-8)",
 }
 
+enum RECORDING_BACKGROUND_COLOR {
+  SELECTED = "var(--mantine-color-indigo-6)",
+  DESELECTED = "var(--mantine-color-indigo-2)",
+}
+
 const TILE_SIZE = "48px";
 const HOVER_SIZE = "52px";
 
 
-export const BaseTile: FC<BaseTileProps> = ({ label, style }) => {
+export const BaseTile: FC<BaseTileProps> = ({ label, style, ...attributes }) => {
   const [isHover, setIsHover] = useState<boolean>(false);
 
-  const handleMouseEnter = () => setIsHover(false); // change this to true after getting things figured out
+  const handleMouseEnter = () => setIsHover(true); // change this to true after getting things figured out
   const handleMouseLeave = () => setIsHover(false);
 
 
   const baseTileStyle: CSSProperties = {
     width: isHover ? HOVER_SIZE : TILE_SIZE,
     height: isHover ? HOVER_SIZE : TILE_SIZE,
+    cursor: "pointer",
     color: "#efefef",
     textAlign: "center",
     lineHeight: isHover ? HOVER_SIZE : TILE_SIZE,
     margin: isHover ? "-2px" : "auto",
-    // transition: 'all .2s', not sure how to get this to work without everything "jumping"
+    // transition: "all .2s", // not sure how to get this to work without everything "jumping"
   };
   return (
-    <div style={{ ...baseTileStyle, ...style }} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+    <div style={{ ...baseTileStyle, ...style }} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} {...attributes}>
         {label}
     </div>
   );
@@ -83,15 +97,79 @@ export const BookTile: FC<BookTileProps> = ({ book, bookId }) => {
   );
 };
 
-export const ChapterTile: FC<ChapterTileProps> = ({ chapter, isRead }) => {
+export const ChapterTile: FC<ChapterTileProps> = ({ bookTitle, chapter, isRead, isRecording, isSelected, toggleTileSelected, beginRecording }) => {
+  const [popoverOpened, setPopoverOpened] = useState<boolean>(false);
+  const ref = useClickOutside(() => setPopoverOpened(false));
+
   const chapterTileStyle: CSSProperties = {
-    "backgroundColor": isRead ? BACKGROUND_COLOR.READ : BACKGROUND_COLOR.UNREAD,
+    backgroundColor: isRead ? BACKGROUND_COLOR.READ : BACKGROUND_COLOR.UNREAD,
   }
+
+  const recordingChapterTileStyle: CSSProperties = {
+    backgroundColor: isSelected ? RECORDING_BACKGROUND_COLOR.SELECTED : RECORDING_BACKGROUND_COLOR.DESELECTED,
+    color: "var(--mantine-color-dark-9)",
+  }
+
+  const handleChapterSelectionChange = () => {
+    toggleTileSelected(chapter);
+  }
+
   return (
-    <Link href="#">
-      <a>
-        <BaseTile label={chapter.toString()} style={chapterTileStyle} />
-      </a>
-    </Link>
-  )
+    <div style={{ margin: "auto", verticalAlign: "middle" }}>
+      {isRecording ? (
+        <BaseTile
+          label={chapter.toString()}
+          style={recordingChapterTileStyle}
+          onClickCapture={handleChapterSelectionChange}
+        />
+      ) : (
+        <Popover
+          closeOnClickOutside={true}
+          opened={popoverOpened}
+          position="bottom"
+          transition="slide-down"
+        >
+          <Popover.Target>
+            <BaseTile
+              label={chapter.toString()}
+              style={chapterTileStyle}
+              onClickCapture={() => setPopoverOpened(true)}
+            />
+          </Popover.Target>
+          <Popover.Dropdown>
+            <Box ref={ref}>
+              <List spacing="xs" size="lg">
+                <List.Item
+                  icon={
+                    <ThemeIcon color={"gray"} size={24} radius="xl">
+                      <IconBook size={24} />
+                    </ThemeIcon>
+                  }
+                >
+                  <Link
+                    href={`https://www.jw.org/en/library/bible/nwt/books/${bookTitle}/${chapter}/`}
+                  >
+                    <a>
+                      Read <IconExternalLink />
+                    </a>
+                  </Link>
+                </List.Item>
+                <List.Item
+                  onClickCapture={beginRecording}
+                  style={{ cursor: "pointer" }}
+                  icon={
+                    <ThemeIcon color={"gray"} size={24} radius="xl">
+                      <IconPencil size={24} />
+                    </ThemeIcon>
+                  }
+                >
+                  <span>Record reading</span>
+                </List.Item>
+              </List>
+            </Box>
+          </Popover.Dropdown>
+        </Popover>
+      )}
+    </div>
+  );
 }
--- pages/book/[id].tsx
+++ pages/book/[id].tsx
@@ -1,46 +1,101 @@
 import { useRouter } from "next/router";
-import { Loader, ActionIcon } from "@mantine/core";
+import { Loader, ActionIcon, Title, Group } from "@mantine/core";
 import GridLayout from "../../components/Grid";
 import { ChapterTile } from "../../components/Tile";
 import { useBook } from "../../hooks/useBooks";
-import styles from "../../styles/Home.module.css";
 import Link from "next/link";
-import { IconArrowLeft } from "@tabler/icons";
+import { IconArrowLeft, IconCheck, IconX } from "@tabler/icons";
+import { useState } from "react";
 
 export const ErrorMessage = () => <div>Uh oh! Something went wrong!</div>
 
-const BookPageHeader = ({ pageTitle }: { pageTitle: string }) => {
+interface BookPageHeaderProps {
+  pageTitle: string;
+  isRecording: boolean;
+  cancelRecording: () => void;
+  saveRecording: () => void;
+}
+
+interface ChapterSelection {
+  [key: number]: boolean;
+}
+
+const BookPageHeader = ({ pageTitle, isRecording, cancelRecording, saveRecording }: BookPageHeaderProps) => {
   return (
     <div style={{ display: "flex", flexDirection: "row" }}>
-      <Link href="/">
-        <ActionIcon title="Go Back" aria-label='Go Back'>
-          <IconArrowLeft size={32} />
-        </ActionIcon>
-      </Link>
-    <h2 className={styles.title} style={{ width: "100%", textAlign: "right" }}>
+      <Group noWrap={true}>
+        <Link href="/">
+          <ActionIcon title="Go Back" aria-label='Go Back'>
+            <IconArrowLeft size={32} />
+          </ActionIcon>
+        </Link>
+        {isRecording && (
+          <>
+            <ActionIcon onClickCapture={cancelRecording} title="Cancel" aria-label="cancel">
+              <IconX size={32} color="red" />
+            </ActionIcon>
+            <ActionIcon onClickCapture={saveRecording} title="Save" aria-label="save">
+              <IconCheck size={32} color="green" />
+            </ActionIcon>
+          </>
+        )}
+      </Group>
+    <Title order={2} style={{ width: "100%", textAlign: "right" }}>
       {pageTitle}
-    </h2>
+    </Title>
   </div>
   )
 }
 
 const BookPage = () => {
+  const [isRecording, setIsRecording] = useState<boolean>(false);
+  const [chapterSelection, setChapterSelection] = useState<ChapterSelection>({});
   const router = useRouter();
   const { id } = router.query;
-
   const { data, isError, isLoading } = useBook(id as string);
+
+  const beginRecording = () => {
+    setIsRecording(true);
+    if (chaptersRead) {
+      const initialSelectedChapters = chaptersRead?.reduce((acc: ChapterSelection, chapter) => {
+        return (acc[chapter]=true, acc)
+      }, {})
+      setChapterSelection(initialSelectedChapters);
+    }
+  }
+  const cancelRecording = () => setIsRecording(false);
+  const saveRecording = () => {
+    setIsRecording(false);
+  }
+  const toggleTileSelected = (chapter: number) => {
+    const updatedChapterSelection = chapterSelection;
+    updatedChapterSelection[chapter] = !chapterSelection[chapter];
+    setChapterSelection(updatedChapterSelection);
+  }
 
   if (isLoading) return <Loader />
   if (isError || !data) return <ErrorMessage />
 
   const { name, chapters, chaptersRead } = data;
+
   const chapterTiles = [];
   for (let i = 1; i <= chapters; i++) {
-    chapterTiles.push(<ChapterTile chapter={i} isRead={chaptersRead?.includes(i) || false} key={i} />)
+    chapterTiles.push(
+      <ChapterTile
+        beginRecording={beginRecording}
+        bookTitle={name}
+        chapter={i}
+        isRead={chaptersRead?.includes(i) || false}
+        isRecording={isRecording}
+        isSelected={!!chapterSelection[i]}
+        key={i}
+        toggleTileSelected={toggleTileSelected}
+      />
+    )
   }
   return (
     <section>
-      <BookPageHeader pageTitle={name} />
+      <BookPageHeader pageTitle={name} isRecording={isRecording} cancelRecording={cancelRecording} saveRecording={saveRecording} />
       <GridLayout>{chapterTiles}</GridLayout>
     </section>
   )
