7ffb8a0e88e4465b1f62603718178e749307d437
Jordan Lamoreaux <jlamoreaux@atlassian.com>
2022-10-26 05:37:30 -0500

    partial update to local storage method

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import { Loader } from "@mantine/core";
 import GridLayout from "../components/Grid";
 import { BookTile } from "../components/Tile";
 import { useAllBooks } from "../hooks/useBooks";
-import useStickyState, { getStickyValue } from "../hooks/useStickyState";
+import useStickyState, { getStickyValue, setStickyValue } from "../hooks/useStickyState";
 import ModalButton from "../components/ModalButton";
 import RecordForm from "../components/RecordForm";
 import { sortNumerically } from "../utils/sortNumerically";
@@ -18,7 +18,11 @@ export type ReadingForm = {
   chapters: number[];
 }
 
-const sortAndRemoveDuplicateChapters = (chapters: number[]) => {
+export type BookRecord = {
+  [chapter: string]:  Date[];
+};
+
+export const sortAndRemoveDuplicateChapters = (chapters: number[]) => {
   chapters = sortNumerically(chapters);
   const alreadySeenChapters: {[chapter: number]: boolean} = {};
   return chapters.filter((chapter) => {
@@ -41,20 +45,36 @@ const Home: NextPage = () => {
     if (!book) {
       return;
     }
+    // Get existing record from local storage.
+    // Doing this twice until ready to cut over to new method.
     const currentRecord = getStickyValue<ReadingRecord>("readingRecord");
-    if (currentRecord && Object.keys(currentRecord).length !== 0) {
-      if (currentRecord[book]) {
-        currentRecord[book] = sortAndRemoveDuplicateChapters([
-          ...currentRecord[book],
-          ...chapters,
-        ]);
-      } else {
-        currentRecord[book] = chapters.sort();
+    const currentBookRecord = getStickyValue<BookRecord>(book);
+
+    let newBookRecord: BookRecord = {};
+
+    const date = new Date();
+    date.setHours(0, 0, 0, 0);
+
+    // Use original localstorage to update new version of local storage
+    if (!currentBookRecord || Object.keys(currentBookRecord).length === 0) {
+      if (currentRecord && currentRecord[book]) {
+          newBookRecord =
+            currentRecord[book].reduce((acc: BookRecord, chapter) => {
+              return (acc[chapter] = [date]), acc;
+            }, {}) || {};
       }
-      setReadingRecord(currentRecord);
-    } else {
-      setReadingRecord({ [book]: sortNumerically(chapters) });
     }
+
+    chapters.forEach(chapter => {
+      if (currentBookRecord && currentBookRecord[chapter]) {
+        if (currentBookRecord[chapter].indexOf(date) !== -1) {
+          newBookRecord[chapter].push(date)
+        }
+      } else {
+        newBookRecord[chapter] = [date]
+      }
+    })
+    setStickyValue(book, newBookRecord);
     setIsDoneRecordingToday(true);
     handleModalClose();
   }
